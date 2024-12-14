885b5e532b44f6a4465715988d278d7842c86d77
Cem <cemkesici51@gmail.com>
2024-12-14 18:20:43 +0300

    Add project files.

---
+++ WeatherApp.API/Controllers/RecommendationController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherApp.API.Controllers
+{
+    public class RecommendationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
